94a5712d24f797d6f4076ecfe391bc1805041feb
harih4ran <80659011+harih4ran@users.noreply.github.com>
2021-03-15 12:14:04 +0530

    Create Login.js

---
+++ src/components/authentication/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import '../../../assets/css/login.css'
+const Login = () => {
+    return (
+        <div className="container">
+            <br/>
+            <div className="card shadow-lg">
+            <div class="form-content register-box">
+            <form>
+                <div class="form-group form-box">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" id="email" placeholder="Email Address" />
+                </div>
+                <div class="form-group form-box">
+                    <label for="pass">Password</label>
+                    <input type="text" class="form-control" id="pass" placeholder="Password" />
+                </div>
+               <div class="forgetpass">
+<Link to="#">Forget Password</Link>
+                <button type="submit" class="btn register-button">Register</button>
+           </form>
+
+<div class="register">
+<Link to="#">Create a new Account</Link>
+            </div>
+            </div>
+        </div>
+    )
+};
+export default Login;
