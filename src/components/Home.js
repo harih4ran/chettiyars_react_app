b50761f100069675e936551ef8970de0eaf3705a
harih4ran <80659011+harih4ran@users.noreply.github.com>
2021-03-15 12:05:22 +0530

    Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,13 +12,13 @@ const Home = () => {
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide" />
+      <img class="d-block w-100" src="https://unsplash.com/photos/oyXis2kALVg" alt="First slide" />
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide" />
+      <img class="d-block w-100" src="https://unsplash.com/photos/oyXis2kALVg" alt="Second slide" />
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide" />
+      <img class="d-block w-100" src="https://unsplash.com/photos/oyXis2kALVg" alt="Third slide" />
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
